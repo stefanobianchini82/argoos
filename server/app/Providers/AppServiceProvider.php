1761a6b6412146e5dd570d86f2bb309eb66d2382
Stefano Bianchini <stefano.bianchini@simplenetworks.it>
2026-05-12 19:07:18 +0200

    Force https in production

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\ChannelManager;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         $this->app->resolving(ChannelManager::class, function (ChannelManager $manager) {
             $manager->extend('telegram', fn () => new TelegramChannel());
             $manager->extend('slack', fn () => new SlackChannel());
